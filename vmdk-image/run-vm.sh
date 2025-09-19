592a31a931f786fd6eca408ae6e47dbf8a1ab4c0
FerRory <rory@ferrory.net>
2025-09-19 10:18:39 +0200

    Add Port Forwarding

--- vmdk-image/run-vm.sh
+++ vmdk-image/run-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # === Configuration ===
-VM_NAME="CentOS9-ARM64-2"
+VM_NAME="bootc-demo-vmdk"
 VM_DIR="$HOME/VirtualBox VMs/$VM_NAME"
 DISK_SIZE_MB=20480   # 20 GB
 MEMORY_MB=4096
@@ -10,7 +10,6 @@ DISK_PATH="output/vmdk/disk.vmdk"
 
 # === Create VM ===
 VBoxManage createvm --name "$VM_NAME" --ostype "Oracle9_arm64" --platform-architecture "arm"  --register --basefolder "$HOME/VirtualBox VMs"
-
 # === Configure VM ===
 VBoxManage modifyvm "$VM_NAME" \
     --memory $MEMORY_MB \
@@ -32,5 +31,11 @@ VBoxManage storagectl "$VM_NAME" --name "SATA Controller" --add sata --controlle
 VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$DISK_PATH"
 #VBoxManage storageattach "$VM_NAME" --storagectl "SATA Controller" --port 1 --device 0 --type dvddrive --medium "$ISO_PATH"
 
+VBoxManage modifyvm $VM_NAME --natpf1 "ssh,tcp,127.0.0.1,2222,10.0.2.15,22"
 # === Start VM ===
 VBoxManage startvm "$VM_NAME"
+
+
+# Fix Disk issue
+# vi /Users/roryschellekens/Library/VirtualBox/VirtualBox.xml
+#
